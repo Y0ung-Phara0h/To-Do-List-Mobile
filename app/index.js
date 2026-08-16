2504b2991e8f01e3e56da051ec9cf406c5e39fe3
Y0ung-Phara0h <263008885+Y0ung-Phara0h@users.noreply.github.com>
2026-08-16 21:33:06 +0300

    Delete app directory

--- app/index.js
+++
@@ -1,9 +0,0 @@
-import { View, Text } from 'react-native'
-import React from 'react'
-import Styles from './src/styles/styles';
-import SignUp from './src/screens/signUp';
-import Login from './src/screens/login';
-
-export default function App() {
-  return <Login/>;
-}
